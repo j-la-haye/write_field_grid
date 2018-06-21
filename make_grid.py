d5eb7a39755f19dcd87d5da8aee04123dbbf2b5d
j-la-haye <jesse.r.murray@gmail.com>
2018-06-21 20:08:55 +0200

    Fixed polygon iterator
    Error was creating invalid shape file. The shape file produced is now valid.

--- make_grid.py
+++ make_grid.py
@@ -1,10 +1,8 @@
-from shapely.geometry.polygon import Polygon 
+from shapely.geometry.polygon import  Polygon 
 from shapely.ops import polygonize
 import fiona
 from fiona.crs import from_epsg
-from shapely.geometry import mapping, LineString, MultiLineString,CAP_STYLE, JOIN_STYLE
-import plot_figures
-from plot_figures import set_limits
+from shapely.geometry import mapping, LineString, MultiLineString,CAP_STYLE, JOIN_STYLE,Polygon
 from shapely.affinity import rotate,affine_transform,translate
 import numpy as np
 from matplotlib import pyplot as plt
@@ -19,7 +17,7 @@ def grid_dim():
     hlines = [((x1, yi), (x2, yi)) for x1, x2 in zip(x[:-1], x[1:]) for yi in y]
     vlines = [((xi, y1), (xi, y2)) for y1, y2 in zip(y[:-1], y[1:]) for xi in x]
     grid = MultiLineString(hlines+vlines)
-    
+        
     return grid
 
 def plot_grid(fig_x,fig_y,title='Test'):
@@ -45,37 +43,22 @@ def rotate_plots(angle,distance=0,fig_x=10,fig_y=10,title='Test'):
     
     t_grid = translate(grid,xo,yo)
     r_grid = rotate(t_grid,angle,origin =(xo,yo))
-    r_plots = list(polygonize(r_grid))
     
-    for poly in r_plots:
-        buf = poly.buffer(-(distance),resolution=1,cap_style=3,join_style=2,mitre_limit=10)
-        bufs.append(buf)
+    plots = list(polygonize(r_grid))
     
-    ######## Plot Grid Lines Only
-    #
-    #fig,ax = plt.subplots(figsize=(fig_x,fig_y))
-    #ax.set_title(title,color='#FFFFFF')
-    #bufs=[]
-    #fp=[]
-    # Plot Grid Lines
-    #for line in r_grid:
-    #    x,y = line.xy
-    #    ax.plot(x,y,color='#000000')   
+    tp = [Polygon(pi) for pi in plots]
     
-    ####### Plot field polygons
-    #
-    #for poly in r_plots:
-    #    x,y=poly.exterior.xy
-    #   img1 = ax.plot(x, y,color='#000000')
-    #    ax.fill(x, y, alpha=0.3, fc='r',color='#6699cc', ec='none')    
-    #    ax.set_title(title,color='#FFFFFF')
-    polys = polygonize(bufs)
-    
-    #plt.show()  
+    for poly in tp:
+        bufs.append(poly.buffer(-distance,
+                                 resolution = 1,
+                                 cap_style=3,
+                                 join_style=2,
+                                 mitre_limit=10)) 
     return bufs,r_grid
 
 
 def plot_buffer(title='Test'):
+    
     fig, ax = plt.subplots(figsize = (10,10))
     
     bufs,grid=rotate_plots(angle,buffer)
@@ -93,9 +76,7 @@ def plot_buffer(title='Test'):
     ax.set_title(title,color='#FFFFFF')
     ax.set_axis_on()
     
-    #return bufs
-    #plt.show()
-
+    
 def read_csv(in_csv):
     data = {}
     data['test_num'] = []
@@ -105,28 +86,24 @@ def read_csv(in_csv):
         for line in reader:
             data['test_num'].append(line[0])
     return data
+            
+def write_shapes(out_dest,index_name):
     
-def write_shp(out_dest):
-
-    out = out_dest
-    schema = {
-        "geometry": "Polygon",
-        "properties": {"id": "int",
-                       "test_num" : 'str' } 
-    }
-    plots,grid = rotate_plots(buffer,angle)
-    c = 0
-    with fiona.open(out, 'w', driver='ESRI Shapefile',schema=schema, crs=('EPSG:32631')) as ds_dst:
-        indices = read_csv(in_csv)
-        for poly in plots:
-            ds_dst.write({'geometry': mapping(poly), 
+    indices = read_csv(in_csv)
+    poly,r_grid =  rotate_plots(angle,buffer)
+    
+    schema = {"geometry": "Polygon","properties": {"id": "int",index_name : 'str' }, }
+    
+   
+    with fiona.open(out_dest, 'w', driver='ESRI Shapefile',schema=schema, crs=('EPSG:32631')) as ds_dst:
+        c = 0
+        for i in poly:
+            ds_dst.write({'geometry': mapping(i), 
                           'properties': {'id': c,
                           'test_num':indices.get('test_num')[c]},
-                         })
-            
+                         })  
             c+=1
-            
-               
+                           
 if __name__ == '__main__':
     
     div_y = 1
@@ -135,11 +112,15 @@ if __name__ == '__main__':
     title = 'Test'
     fig_x = 10
     fig_y = 10
-    angle = 75
+    angle = 30
     buffer = 0.5
     xo,yo = 584435,5696135
     in_csv = 'test.csv'
     
-    field = plot_grid(fig_x,fig_y)
-    plots = plot_buffer('Test_Plots')
-    shape = write_shp('/Users/jesselahaye/Documents/gisTools/Tier_1/test_plot6.shp')
+    ## Note: index column name argument must 
+    # agree with the name in the supplied csv file. 
+    
+    write_shapes('shapes.shp','test_num')  
+    #field = plot_grid(fig_x,fig_y)
+    #plots = plot_buffer('Test_Plots')
+    
